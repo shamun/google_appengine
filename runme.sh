72d69491e6a9048cabd66cd5087382e09f5108ef
root <root@sun-M14xR2>
2013-07-05 05:17:34 +0200

    Update

---
+++ runme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Video executing..."
+
+### Old - has no &media=audio
+#/opt/python2.7/bin/python2.7 /var/tmp/google_appengine/dev_appserver.py --host 0.0.0.0 /var/tmp/webrtc-samples-read-only/apprtc &
+
+### New - has &media=audio
+#/opt/python2.7/bin/python2.7 /var/tmp/google_appengine/dev_appserver.py --host 0.0.0.0 /var/tmp/webrtc-read-only/samples/js/apprtc &
+
+### New
+/opt/python2.7/bin/python2.7 /var/tmp/google_appengine/dev_appserver.py --host 0.0.0.0 /var/tmp/google_appengine/www/webrtc-read-only/samples/js/apprtc &
